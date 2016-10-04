910d2ff28eaa11add4cbeb74021034f75d3e5d3a
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 17:58:02 -0700

    Allows user to updateexisting rating

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,11 +1,21 @@
 # your code goes here
 from sys import argv
+from random import choice
 
 filename = argv[1]
 
 text = open(filename)
 
 ratings = {}
+
+user_prompt = """
+Press A if you want to see all of the restaurant ratings,
+B if you would like to add a new restaurant,
+C if you would like to quit,
+D if you would like to update a random restaurant rating,
+E if you would like to choose restaurant to update.
+
+>> """
 
 def print_alphabetically(dictionary_to_unpack):
     """Prints dictionary alphabetically"""
@@ -21,9 +31,7 @@ for line in text:
     ratings[restaurant] = int(rate)
 
 while True:
-    user_input = raw_input("\nPress A if you want to see all of the restaurant ratings,\n"
-                           "B if you would like to add a new restaurant, \n"
-                           "or C if you would like to quit.\n >> ").lower()
+    user_input = raw_input(user_prompt).lower()
     if user_input == "a":
         print_alphabetically(ratings)
     elif user_input == "b":
@@ -34,6 +42,17 @@ while True:
     elif user_input == "c":
         print "You are now exiting the Rating Application. Good Bye!"
         break
+    elif user_input == "d":
+        random_restaurant = choice(ratings.keys())
+        print random_restaurant, ratings[random_restaurant]
+        updated_restaurant_rating = int(raw_input('Updated restaurant rating: '))
+        ratings[random_restaurant] = updated_restaurant_rating
+        print_alphabetically(ratings)
+    elif user_input == "e":
+        chosen_restaurant = raw_input("What restaurant would you likt to update? ")
+        updated_restaurant_rating = int(raw_input('Updated restaurant rating: '))
+        ratings[chosen_restaurant] = updated_restaurant_rating
+        print_alphabetically(ratings)
     else:
         print "Please enter a valid input."
 
