208bcbc71c0b37fa2d6e585570a387a3232d0b67
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 17:11:14 -0700

    Allows user to add ratings. Resorts dict.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -8,8 +8,18 @@ text = open(filename)
 ratings = {}
 for line in text:
     ratings_list = line.rstrip().split(':')
-    ratings[ratings_list[0]] = ratings.get(ratings_list[0],int(ratings_list[1]))
+    ratings[ratings_list[0]] = int(ratings_list[1])
 
 for restaurant, rate in sorted(ratings.iteritems()):
     print "{} is rated at {}." .format(restaurant, rate)
 
+user_restaurant = raw_input("What restaurant would you like to add? ")
+user_rating = int(raw_input("What rating would you give that restaurant? "))
+
+ratings[user_restaurant] = user_rating
+
+for restaurant, rate in sorted(ratings.iteritems()):
+    print "{} is rated at {}." .format(restaurant, rate)
+
+
+    
